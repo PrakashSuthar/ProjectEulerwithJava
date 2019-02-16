68472bf088299b268b914c9c4065f8a7586bec9b
PrakashSuthar <31180283+PrakashSuthar@users.noreply.github.com>
2019-02-16 18:00:12 +0530

    Update SumMultiplesOfThreeAndFive.java

--- SumMultiplesOfThreeAndFive.java
+++ SumMultiplesOfThreeAndFive.java
@@ -15,19 +15,16 @@ public class SumMultiplesOfThreeAndFive{
             long div5=(n-1)/5;
             long div15=(n-1)/15;
             long sum3=0,sum5=0,sum15=0;
-            if(div3==1){
+            if(div3==1)
                 sum3=3;
-            }
             else
                 sum3=((3+3*div3)*div3)/2;
-            if(div5==1){
+            if(div5==1)
                 sum5=5;
-            }
             else
                 sum5=((5+5*div5)*div5)/2;
-            if(div15==1){
+            if(div15==1)
                 sum15=15;
-            }
             else
                 sum15=((15+15*div15)*div15)/2;
             long sum_of_3_5prime=sum3+sum5-sum15;
